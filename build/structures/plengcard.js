cad4af1343b5e4ef68d949ad475d8e8283c20c28
KARONAGAMING <86901628+KARONAGAMING@users.noreply.github.com>
2024-02-28 14:59:49 +0700

    Update plengcard.js

--- build/structures/plengcard.js
+++ build/structures/plengcard.js
@@ -142,7 +142,7 @@ class plengcard {
             circleCtx.fillStyle = `#${validatedColor}`;
             circleCtx.fill();
 
-            const background = await canvas.loadImage(`https://media.discordapp.net/attachments/1193048344822566995/1206548639305170974/plengbanner-khmernewyear.png?ex=65dc68fc&is=65c9f3fc&hm=f113158c30e5d504d45d746b5370b86832df0601248b3cfcd52b833adf75c12c`);
+            const background = await canvas.loadImage(`https://media.discordapp.net/attachments/1193048344822566995/1206548639305170974/plengbanner-khmernewyear.png?ex=65eeddfc&is=65dc68fc&hm=29b1bfb7a4b2e964f1774bc840125b1c0ffbee6af0f7c1a8a4e6a813379adfbe`);
 
             const thumbnailCanvas = canvas.createCanvas(564, 564);
             const thumbnailCtx = thumbnailCanvas.getContext('2d');
